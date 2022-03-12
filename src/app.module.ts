11f780858dbad4fc86e0bd8ae67f5aa3fbf1116f
Nag <nagcloudlab@gmail.com>
2022-03-12 13:53:26 +0530

    interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from './photos/photo.entity';
 import { PhotosModule } from './photos/photos.module';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot({
             type: "sqlite",
             database: "db.sqlite",
-            entities: [Photo],
+            entities: [Photo, User],
+            synchronize: true
         }),
-        PhotosModule
+        PhotosModule,
+        UsersModule
     ]
 })
 export class AppModule { }
